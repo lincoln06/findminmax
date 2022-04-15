b14335db866a145e6d3339fd254f9cff7a26dcbc
MJL <maciekjl1993@gmail.com>
2022-04-15 18:46:39 +0200

    not working, but very close

--- ciag_minmax/Program.cs
+++ ciag_minmax/Program.cs
@@ -15,14 +15,47 @@ namespace ciag_minmax
             Random random = new Random();
             for(int i=0;i<length; i++)
             {
-                inputArray[i] = random.Next(100);
+                inputArray[i] = random.Next(20);
             }
-            if (arrLength != length) inputArray[arrLength - 1] = inputArray[length - 1]; 
-            for(int j=0;j<arrLength;j++)
+            
+            if (arrLength != length) inputArray[arrLength - 1] = inputArray[length - 1];
+            //int[] outputArray = SortTable(inputArray);
+            
+            for (int j = 0; j < arrLength; j++)
             {
                 Console.Write($"{inputArray[j]} ");
             }
+            
+            
             Console.ReadKey();
+            int[] minMaxArray = FindMinMax(inputArray, arrLength);
+        }
+        static int[] FindMinMax(int[] inputArray, int arrLength)
+        {
+            int buffer = 0;
+            SortTable(inputArray); //sortowanie par w tabeli rosnąco
+            if (inputArray.Length == 2) return inputArray; //jeśli wyjściowa tabela ma 2 elementy, znaleźliśmy min i max
+            int outputArrLength = arrLength / 2; //wyjściowa tablica po każdej rekurencji będzie miała 2 razy mniej elementów
+            int[] outputArray = new int[outputArrLength];
+            for(int i=0;i<arrLength; i=i+2)
+            {
+                
+            }
+        }
+        static int[] SortTable(int[] inputArray)
+        {
+            int buffer = 0;
+            for(int i=0;i<inputArray.Length;i=i+2)
+            {
+                if(inputArray[i]>inputArray[i+1])
+                {
+                    buffer = inputArray[i];
+                    inputArray[i] = inputArray[i + 1];
+                    inputArray[i + 1] = buffer;
+                }
+               
+            }
+            return inputArray;
         }
     }
 }
