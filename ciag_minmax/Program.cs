e1500c3a3c98922a14845709cb1483d1fec9076c
MJL <maciekjl1993@gmail.com>
2022-04-16 18:54:29 +0200

    100% working

--- ciag_minmax/Program.cs
+++ ciag_minmax/Program.cs
@@ -1,74 +1,94 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace ciag_minmax
 {
     class Program
     {
+        static int numberOfFindOperations = 0;
         static void Main(string[] args)
         {
-            Console.WriteLine("Ile elementów ma mieć ciąg liczbowy?");
+            Console.WriteLine("How many elements?");
             int arrLength = int.Parse(Console.ReadLine());
-            int length = arrLength;
-            if (arrLength % 2 != 0) arrLength++;
-            int[] inputArray = new int[arrLength];
             Random random = new Random();
-            for(int i=0;i<length; i++)
+            int[] inputArray = new int[arrLength];
+            for (int i = 0; i < arrLength; i++)
             {
-                inputArray[i] = random.Next(20);
+                inputArray[i] = random.Next(300);
+                Console.Write($"{inputArray[i]}\t");
             }
-            
-            if (arrLength != length) inputArray[arrLength - 1] = inputArray[length - 1];
-            //int[] outputArray = SortTable(inputArray);
-            
-            for (int j = 0; j < arrLength; j++)
-            {
-                Console.Write($"{inputArray[j]} ");
-            }
-            inputArray=SortTable(inputArray);
-            
+            Console.WriteLine();
+
             Console.ReadKey();
-            int[] minMaxArray = FindMinMax(inputArray, arrLength);
-            Console.WriteLine("\n");
-            for(int i=0;i<minMaxArray.Length;i++)
-            {
-                Console.Write($"{minMaxArray[i]} ");
-            }
+            int[] outputArray = FindMinMax(inputArray);
+            Console.WriteLine($"Ilość porównań: {numberOfFindOperations}");
+            Console.WriteLine($"Element minimalny: {outputArray[0]}");
+            Console.WriteLine($"Element maksymalny: {outputArray[1]}");
             Console.ReadKey();
         }
-        static int[] FindMinMax(int[] inputArray, int inputArrLength)
+        static int[] FindMinMax(int[] inputArray)
         {
-            //sortowanie par w tabeli rosnąco
-            inputArray=SortTable(inputArray);
-            if (inputArrLength == 2) return inputArray;
-            
-            int outputArrIndex = 0;
-            int outputArrLength = inputArrLength / 2;
-            int[] outputArray = new int[outputArrLength];
-            for (int i=0;i<inputArrLength-4;i+=4)
+            inputArray = SortTable(inputArray);
+            if (inputArray.Length == 2) return inputArray;
+
+            if (inputArray.Length % 4 != 0) inputArray = AddMoreElements(inputArray);
+            int[] outputArray = new int[inputArray.Length / 2];
+            int j = 0;
+            for (int i = 0; i < inputArray.Length; i += 4)
             {
-                if (inputArray[i] < inputArray[i + 2]) outputArray[outputArrIndex] = inputArray[i];
-                else outputArray[outputArrIndex] = inputArray[i + 2];
-                if (inputArray[i+1] > inputArray[i + 3]) outputArray[outputArrIndex+1] = inputArray[i+1];
-                else outputArray[outputArrIndex+1] = inputArray[i + 3];
-                outputArrIndex += 2;
+                outputArray[j] = FindMin(inputArray[i], inputArray[i + 2]);
+                outputArray[j + 1] = FindMax(inputArray[i + 1], inputArray[i + 3]);
+                j += 2;
             }
-            return FindMinMax(outputArray, outputArray.Length);
+            return FindMinMax(outputArray);
         }
+
         static int[] SortTable(int[] inputArray)
         {
             int buffer = 0;
-            for(int i=0;i<inputArray.Length-1;i=i+2)
+            for (int i = 0; i < inputArray.Length - 1; i = i + 2)
             {
-                if(inputArray[i]>inputArray[i+1])
+                if (inputArray[i] > inputArray[i + 1])
                 {
                     buffer = inputArray[i];
                     inputArray[i] = inputArray[i + 1];
                     inputArray[i + 1] = buffer;
                 }
-               
+
             }
             return inputArray;
+        }
+        static int FindMin(int a, int b)
+        {
+            numberOfFindOperations++;
+            if (a > b) return b;
+            return a;
+        }
+        static int FindMax(int a, int b)
+        {
+            numberOfFindOperations++;
+            if (a > b) return a;
+            return b;
+        }
+        static int[] AddMoreElements(int[] inputArray)
+        {
+            int outputArrLength = inputArray.Length;
+
+            while (outputArrLength % 4 != 0)
+            {
+                outputArrLength++;
+            }
+            int[] outputArray = new int[outputArrLength];
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                outputArray[i] = inputArray[i];
+            }
+
+            while (outputArrLength > inputArray.Length)
+            {
+                outputArray[outputArrLength - 1] = outputArray[inputArray.Length - 1];
+                outputArrLength--;
+            }
+            return outputArray;
         }
     }
 }
